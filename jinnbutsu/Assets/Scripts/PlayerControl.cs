472eec5f3d0e23d426a98c52110a2797eab006cb
lch27 <ts521989@gmail.com>
2022-10-25 19:25:06 +0800

    人物走路程式檔完成

---
+++ jinnbutsu/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    CharacterController cc;
+   public  float speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(h, 0, v);
+
+        //旋轉
+        if (dir.magnitude > 0.1f)
+        {
+            float faceAngle = Mathf.Atan2(h, v) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0, faceAngle, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 0.3f);
+        }
+        
+        //移動
+        Vector3 move = dir * speed;
+
+        //加入地心引力
+        if (!cc.isGrounded)
+        {
+            move.y = -9.8f * 100 *Time.deltaTime;
+        }
+        cc.Move (move * speed * Time.deltaTime);
+
+    }
+}
